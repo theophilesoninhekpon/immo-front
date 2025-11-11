fdeebd8791744c2dc0aa3fe2dc4ac12221da0ad0
Theophile Soninhekpon <theophilesoninhekpon@gmail.com>
2025-11-11 12:23:15 +0100

    prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api'
+  // apiUrl: 'http://localhost:8000/api'
+  apiUrl: 'https://farouche-cytotropic-renee.ngrok-free.dev/api'
 };
 
