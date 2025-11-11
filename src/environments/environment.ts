7d1395ce436830fd11d9f8c7c4cd67daa864ba1d
Theophile Soninhekpon <theophilesoninhekpon@gmail.com>
2025-11-11 13:11:28 +0100

    go

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8000/api'
-  apiUrl: 'https://cool-nights-sit.loca.lt/api'
+  apiUrl: 'https://pens-aqua-stats-when.trycloudflare.com/api'
 };
 
