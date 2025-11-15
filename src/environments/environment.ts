d7b76049f4228d177cd20bf69e5add88d22d74df
Theophile Soninhekpon <theophilesoninhekpon@gmail.com>
2025-11-15 16:33:27 +0100

    build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://immo-api-qeq4.onrender.com/api',
+  apiUrl: 'https://immo-api-adab.onrender.com/api',
   supabaseStorageUrl: 'https://kjldjxuidpgcrffukpkc.supabase.co'
-
 };
 
